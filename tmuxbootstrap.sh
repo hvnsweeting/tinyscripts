b2c92621f9912fbadff9fe6667e07527a575fc42
HVN <hvnsweeting@gmail.com>
2013-02-20 21:02:15 +0700

    move mon , iftop to higher window

--- tmuxbootstrap.sh
+++ tmuxbootstrap.sh
@@ -4,9 +4,9 @@ FRONTEND=byobu
 ACTIVE_IFACE=$(ip li | grep 'state UP' | head -n 1 | awk '{print $2}' | tr -d ':')
 
 $FRONTEND new -d -s hvn
-$FRONTEND neww -t hvn:1 -n 'mon' 'top'
-$FRONTEND splitw -t hvn:1 'tail -f /var/log/syslog | ccze'
-$FRONTEND neww -t hvn:2 -n 'iftop' "sudo iftop -i $ACTIVE_IFACE"
+$FRONTEND neww -t hvn:6 -n 'mon' 'top'
+$FRONTEND splitw -t hvn:6 'tail -f /var/log/syslog | ccze'
+$FRONTEND neww -t hvn:7 -n 'iftop' "sudo iftop -i $ACTIVE_IFACE"
 $FRONTEND selectw -t hvn:0 
 
 $FRONTEND -2 attach -t hvn
