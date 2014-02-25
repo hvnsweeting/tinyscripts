55a405201a096e1ac5823bf210f0fc8794491759
hvnsweeting <hvnsweeting@gmail.com>
2014-02-25 08:56:29 +0700

    add script help writing FAMILUG blog posts

---
+++ blogger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# by Hung Nguyen Viet <hvnsweeting@gmail.com>
+# Tue Feb 25 08:56:14 ICT 2014
+
+# A script helps writing blog post on FAMILUG
+
+echo "Written and tested on Kennel: $(uname -r)"
+
+# -l for linux package
+# -p for python package
