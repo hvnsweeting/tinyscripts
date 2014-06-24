eadca76b1899179bab433014e5d5ba178887b254
Hung Nguyen Viet <hvnsweeting@gmail.com>
2014-06-24 14:25:01 +0800

    fix session name as OS X requires that, do not run top on OSX as it consumes much resource

--- tmux/main.sh
+++ tmux/main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-SES='main.tmux'
+SES='tmuxws'
 
 tmux new -d -s $SES
 tmux neww -t $SES:1
+if [ "$(uname -s)" != 'Darwin' ]; then
 tmux neww -t $SES:2 'top'
+fi
 tmux neww -t $SES:3 -n 'ipython' '. ~/python2/bin/activate; ipython'
 tmux neww -t $SES:4 'mocp'
 tmux selectw -t $SES:0
