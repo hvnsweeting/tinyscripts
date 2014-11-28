07c0619b5a318017523f4f82dc231b9f4bc5ac8d
Hung Nguyen Viet <hvn@familug.org>
2014-11-28 08:10:51 +0700

    merge readme to script

--- vimsalt.sh
+++ vimsalt.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# saltvimplus
+# ===========
+#
+# A simple vim setup ready for writing saltstack.org states
+#
+# This vim setup support:
+#
+# - yaml.vim : vim plugin that helps highlight YAML syntax, jinja files
+#
+# - Snippet.vim: supports snippets for quickly write new state.
+#
+# - A simple vimrc
+
 echo "This install script requires: vim git"
 #apt-get install -y vim git
 
