b7f8f73f5175e121ac217d4efb22ce3afb8bb6b0
Hung Nguyen Viet <hvn@familug.org>
2014-11-28 08:28:43 +0700

    require arg, replace wget with python2, write notification to ~/README

--- saltdevenv.sh
+++ saltdevenv.sh
@@ -6,7 +6,12 @@
 #if [ $EUID -ne 0 ]; then
 #    echo "This script must run by root"
 #fi
-dest_dir=saltdev
+if [ -z $1 ]; then
+    echo "USAGE: $0 path_to_create_devsalt_env"
+    exit 1
+fi
+
+dest_dir=$1
 if [ -e $dest_dir ]; then
     echo `pwd`/"$dest_dir is already existed, remove it"
     exit 1
@@ -28,7 +33,13 @@ if [ -z $code_only ]; then
     git clone git://github.com/saltstack/salt.git mainstream
     pip install -e mainstream
 else
-    wget https://github.com/saltstack/salt/archive/develop.tar.gz
+    python2 -c 'import urllib; urllib.urlretrieve("https://github.com/saltstack/salt/archive/develop.tar.gz", "develop.tar.gz")'
     tar xzf develop.tar.gz
     pip install -e salt-develop
 fi
+
+if ( ! grep SALTDEVENV ~/README |& > /dev/null ); then
+    echo "_SALTDEVENV_ installed at $date, use ``source $dest_dir/bin/activate`` to active venv for dev SaltStack``" >> ~/README
+fi
+echo "Use ``source $dest_dir/bin/activate`` to active venv for dev SaltStack``"
+
