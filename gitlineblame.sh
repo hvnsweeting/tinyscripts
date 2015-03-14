a0d93fc6810bb22f64ba9cb3193d251fc7237cb2
Hung Nguyen Viet <hvnsweeting@gmail.com>
2015-03-14 18:07:39 +0700

    add tool to find who introduced a line in a file

---
+++ gitlineblame.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# by Viet Hung Nguyen <hvn@familug.org>
+# find the commit introduce a piece of content (one word, or a line of code).
+
+FILE=$1
+KEYWORD="$2"
+
+echo "Looking for the commit on $FILE who introduced content: $KEYWORD"
+for hash in `git log --format='%H' $FILE`; do
+    git show $hash:$FILE | grep -q "$KEYWORD" && last=$hash
+done
+git log -n1 $last
