ff2775508440f96f296821d228bb506c92d9674f
Hung Nguyen Viet <hvn@familug.org>
2015-05-23 14:47:19 +0700

    reorganize

--- tmux/main.sh
+++ tmux/main.sh
@@ -3,12 +3,13 @@
 SES='tmuxws'
 
 tmux new -d -s $SES
-tmux neww -t $SES:1
-if [ "$(uname -s)" != 'Darwin' ]; then
-tmux neww -t $SES:2 'top'
-fi
+tmux neww -t $SES:1 -n 'rbi'
+tmux split-window
+tmux neww -t $SES:2
+tmux split-window
 tmux neww -t $SES:3 -n 'ipython' '. ~/python2/bin/activate; ipython'
 tmux neww -n 'mrmime' -t $SES:4 'python /Users/hvn/Github/mrmime/mrmime/__init__.py'
-tmux selectw -t $SES:0
+tmux split-window
 
+tmux selectw -t $SES:1
 tmux -2 attach -t $SES
