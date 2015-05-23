7c5e12df7386047fef7b4d0e369a11f617845527
Hung Nguyen Viet <hvn@familug.org>
2015-05-23 13:41:16 +0700

    run mrmine in separate window

--- tmux/main.sh
+++ tmux/main.sh
@@ -8,7 +8,7 @@ if [ "$(uname -s)" != 'Darwin' ]; then
 tmux neww -t $SES:2 'top'
 fi
 tmux neww -t $SES:3 -n 'ipython' '. ~/python2/bin/activate; ipython'
-tmux neww -t $SES:4 'mocp'
+tmux neww -n 'mrmime' -t $SES:4 'python /Users/hvn/Github/mrmime/mrmime/__init__.py'
 tmux selectw -t $SES:0
 
 tmux -2 attach -t $SES
