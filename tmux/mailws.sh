8b0c7f6936c542b3992e71dda9fd6b36aae7c0ff
Hung Nguyen Viet <hvn@familug.org>
2015-05-23 13:42:50 +0700

    obsoleted

--- tmux/mailws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FRONTEND=tmux
-SESSION=mail
-
-$FRONTEND new -d -s $SESSION
-$FRONTEND neww -t $SESSION:1 -n 'hvnsweeting' "mutt -F ~/mails/hvnsweeting"
-$FRONTEND -2 attach -t $SESSION
