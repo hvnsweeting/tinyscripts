788c88550b3d399de502331ef144e20c8ab12742
Hung Nguyen Viet <hvn@familug.org>
2016-07-02 00:06:52 +0700

    turn to a mass password generator
    - remove quotes from password
    - allow create password for multiple users
    - run external command with given password - may use for hashing

--- genpasswd.py
+++ genpasswd.py
@@ -1,19 +1,46 @@
+import argparse
 import string
 import random
+
+
+PUNC_NO_QUOTE = string.punctuation.replace('"', '').replace("'", "")
+
 
 def gen_passwd(length=16):
     result = []
     result.append(random.choice(string.ascii_lowercase))
     result.append(random.choice(string.ascii_uppercase))
     result.append(random.choice(string.digits))
-    result.append(random.choice(string.punctuation))
+    result.append(random.choice(PUNC_NO_QUOTE))
     for i in range(length-len(result)):
         result.append(random.choice(
             string.ascii_letters +
-            string.digits + string.punctuation))
+            string.digits + PUNC_NO_QUOTE))
     random.shuffle(result)
     return ''.join(result)
 
 
+def main():
+    argp = argparse.ArgumentParser()
+    argp.add_argument('users', nargs='*')
+    argp.add_argument('-c', '--command')
+    args = argp.parse_args()
+    if not args.users:
+        print gen_passwd()
+        return
+
+    if args.command:
+        import shlex
+        cmd = shlex.split(args.command)
+
+    for username in args.users:
+        passwd = gen_passwd()
+        print '{0}: {1}'.format(username, passwd)
+        if args.command:
+            cmd = shlex.split(args.command)
+            import subprocess
+            print subprocess.check_output(cmd + [passwd])
+
+
 if __name__ == "__main__":
-    gen_passwd()
+    main()
