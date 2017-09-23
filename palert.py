0b67c1e92edbd5f64e765dea1170b150b2b76de4
Viet Hung Nguyen <hvn@familug.org>
2017-09-23 11:54:38 +0700

    Add palert

---
+++ palert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Send notification when there are processes use more than THRESHOLD MiB
+of memory
+Tested on Python 3.5
+'''
+import argparse
+import subprocess as spr
+from collections import defaultdict
+
+import psutil
+
+
+argp = argparse.ArgumentParser()
+argp.add_argument("threshold", default=1024, type=int, nargs='?')
+args = argp.parse_args()
+
+THRESHOLD = args.threshold
+
+msgs = []
+name_sizes = defaultdict(int)
+
+for process in psutil.process_iter():
+    rss = process.memory_info().rss
+    size_in_mib = rss / 1024 / 1024
+
+    # TODO
+    # this is wrong when we run python scripts, they will all add up
+    # but okay for now
+    name = process.name().split()[0][:15]
+    name_sizes[name] += size_in_mib
+
+for name, size in name_sizes.items():
+    if size > THRESHOLD:
+        msg = "{}: {} MiB".format(name, size)
+        msgs.append(msg)
+
+if msgs:
+    notification = '\t'.join(msgs)
+    print("Notification", notification)
+    spr.call(['notify-send', notification])
