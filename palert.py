8d9c61ea8127725fd49aecfb227f14e613c06062
Viet Hung Nguyen <hvn@familug.org>
2017-09-23 23:39:21 +0700

    Add logging

--- palert.py
+++ palert.py
@@ -4,14 +4,23 @@
 '''
 Send notification when there are processes use more than THRESHOLD MiB
 of memory
-Tested on Python 3.5
+Tested on Python 3.5, Ubuntu 16.04
 '''
 import argparse
+import logging
+import os
 import subprocess as spr
+
 from collections import defaultdict
 
 import psutil
 
+logging.basicConfig(filename=os.path.expanduser("~/.palert.log"),
+                    level=logging.INFO,
+                    format="%(asctime)s:" + logging.BASIC_FORMAT
+                    )
+
+logger = logging.getLogger('palert')
 
 argp = argparse.ArgumentParser()
 argp.add_argument("threshold", default=1024, type=int, nargs='?')
@@ -29,6 +38,7 @@ for process in psutil.process_iter():
     # TODO
     # this is wrong when we run python scripts, they will all add up
     # but okay for now
+    # read process.name?? for more detail why use first 15 chars
     name = process.name().split()[0][:15]
     name_sizes[name] += size_in_mib
 
@@ -42,4 +52,7 @@ if msgs:
     # If call from cron, must set DISPLAY so notify-send knows where to send
     import os
     os.environ['DISPLAY'] = ':0'
+    logger.info(notification)
     spr.call(['/usr/bin/notify-send', notification])
+else:
+    logger.debug('No process that consumes lot of memory')
